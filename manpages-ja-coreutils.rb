dac3ce1e1cf762c673aa81b65a3cfc22055bc77f
KOSHIKAWA Kenichi <reishoku.misc@pm.me>
2022-05-05 13:01:43 +0900

    fix syntax of branch name
    Signed-off-by: KOSHIKAWA Kenichi <reishoku.misc@pm.me>

--- manpages-ja-coreutils.rb
+++ manpages-ja-coreutils.rb
@@ -1,7 +1,7 @@
 class ManpagesJaCoreutils < Formula
   desc "Japanese manpages of GNU coreutils, translated by JM Project"
   homepage "https://linuxjm.osdn.jp"
-  head "https://github.com/reishoku/manpages-ja-coreutils.git" :branch main
+  head "https://github.com/reishoku/manpages-ja-coreutils.git", branch: "main"
 
   # This formula depends on "man", but the command exist both on macOS and Linux (distros).
 
